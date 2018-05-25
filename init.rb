86aa4a50679b42e2f3577384a1b4903149327056
Eugene G <sonniimukh@gmail.com>
2018-05-25 10:29:09 -0400

    Optional current assignee, formatting adjustments, russian translation

--- init.rb
+++ init.rb
@@ -2,16 +2,17 @@ require_dependency 'Issues/patches/IssueController_patch'
 
 Redmine::Plugin.register :redmine_filter_assignable do
   name 'Redmine Filter Assignable plugin'
-  author 'Luis Roa'
+  author 'Luis Roa, edits by Eugene G'
   description 'Plugin to filter assignable users based on workflow'
-  version '0.0.1'
-  url 'https://github.com/luisr9/redmine_filter_assignable'
+  version '0.0.2'
+  url 'https://github.com/sonniimukh/redmine_filter_assignable'
   author_url 'www.google.com/+LuisRoa'
 
   settings  :partial => 'settings/redmine_filter_assignable_settings',
     :default => {
-      'filtera_users' => 'true',
-      'filtera_autor' => 'true',
-      'filtera_groups' => 'true'     
+      'allow_users' => 'true',
+      'allow_groups' => 'true',
+      'include_author' => 'false',
+      'include_assignee' => 'false',
     }
 end
--- lib/Issues/patches/IssueController_patch.rb
+++ lib/Issues/patches/IssueController_patch.rb
@@ -3,31 +3,25 @@ require_dependency 'issue'
 class Issue
   
   def assignable_users_with_filter_assignable      	
-	
-	types = nil
-	if Setting.plugin_redmine_filter_assignable['filtera_users']  == "true" && Setting.plugin_redmine_filter_assignable['filtera_groups']  == "true" 
-		types = ['User','Group'] 
-	elsif Setting.plugin_redmine_filter_assignable['filtera_users']  == "true" 
-		types = ['User']
-	elsif Setting.plugin_redmine_filter_assignable['filtera_groups']  == "true"
-		types = ['Group']
-	end
-    
-    if types.nil?
-    	users = []
-    	users << author if author && author.active? && Setting.plugin_redmine_filter_assignable['filtera_autor'] == "true"
-	    users << assigned_to if assigned_to
-	    users.uniq.sort	    
-    elsif
-    	users = project.assignable_users.where(:type => types).where("role_id IN (?)",
-	    WorkflowTransition.where(:tracker_id => tracker_id, :old_status_id => status_id).select(:role_id).distinct.map { |e| e.role_id }
-	    ).to_a
-	    users << author if author && author.active? && Setting.plugin_redmine_filter_assignable['filtera_autor'] == "true"
-	    users << assigned_to if assigned_to
-	    users.uniq.sort
+
+    users = []
+    users << author      if author      && author.active?      && Setting.plugin_redmine_filter_assignable['include_author'] == "true"
+    users << assigned_to if assigned_to && assigned_to.active? && Setting.plugin_redmine_filter_assignable['include_assignee'] == "true"
+
+    types = []
+    types << 'User'  if Setting.plugin_redmine_filter_assignable['allow_users']  == "true" 
+    types << 'Group' if Setting.plugin_redmine_filter_assignable['allow_groups']  == "true"
+    if !types.empty?
+      users += project
+        .assignable_users
+        .where(:type => types)
+        .where("role_id IN (?)", WorkflowTransition.where(:tracker_id => tracker_id, :old_status_id => status_id).select(:role_id).distinct.map { |e| e.role_id })
+        .to_a
     end
+    
+    users.uniq.sort
   end
 
   alias_method_chain :assignable_users, :filter_assignable
-end
 
+end
